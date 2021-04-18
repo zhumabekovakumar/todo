9042196a0c9e9864e8f3a0aa2b8b36951a1fd0ad
Kumar Zhumabekova <zhumabekovakumar@gmail.com>
2021-04-18 15:05:26 +0600

    commit

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+
+def homepage(request):
+    return HttpResponse("hello world")
+
+
+def test (request):
+    return render(request, "test.html")
+
+
